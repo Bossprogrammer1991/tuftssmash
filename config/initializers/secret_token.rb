0288bfe2af431685cfa149db3a52264eb203b5cb
Daniel <daniel.griffin@tufts.edu>
2014-05-07 14:29:01 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuftssmash::Application.config.secret_key_base = 'd506a797883c36cecbfd2a5fdc13d426f9064fca2dbc93cff08d79c0e6a24f94155f99ff34a56d904f0779ab03abb999e40724b0d0f3ef9f586ac2bf2f5c4714'
