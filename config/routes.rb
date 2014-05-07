74c24c8dc8e275bc82fcb1d982c32491389e3900
Daniel <daniel.griffin@tufts.edu>
2014-05-07 14:54:14 -0400

    added post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tuftssmash::Application.routes.draw do
 	root 'welcome#index'
+	post 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
